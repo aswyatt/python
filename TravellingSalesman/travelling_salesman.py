26e4de2ee5a2ad378f1e2aa39936e5ecc25056e7
Adam Wyatt <adam.wyatt@stfc.ac.uk>
2022-03-21 09:08:03 +0000

    Just starting

---
+++ TravellingSalesman/travelling_salesman.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+
+class TravellingSalesman:
+    def __init__(self) -> None:
+        pass
+
+    def InitialiseRandomLocations(
+        self, __number:int, size:Tuple[int, int]) -> None:
+        pass
