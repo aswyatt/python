60c9f66f1e9bf0619cf859a3e6136a6338f29596
Adam Wyatt <adam.wyatt@stfc.ac.uk>
2022-03-23 00:01:33 +0000

    kinda working, but not

--- TravellingSalesman/travelling_salesman.py
+++ TravellingSalesman/travelling_salesman.py
@@ -2,6 +2,8 @@ import sys
 from typing import Tuple, List
 import numpy as np
 import random
+import math
+import matplotlib.pyplot as plt
 
 class Point2D:
     """ Defines point in 2D space
@@ -19,7 +21,7 @@ class Point2D:
         return dist
 
     def __repr__(self) -> str:
-        return "(" + str(self.x) + "," + str(self.y) + ")"
+        return "(" + str(round(self.x, 2)) + "," + str(round(self.y, 2)) + ")"
 
 
 class Route:
@@ -34,38 +36,62 @@ class Route:
         self.distance = self._calc_distance()
         self.fitness = self._calc_fitness()
 
+    def __repr__(self) -> str:
+        Str = "["
+        for p in self.route:
+            Str += str(p)
+        Str += "]"
+        return Str
+
     def _calc_distance(self, index=None) -> float:
         """ Calculate distance to traverse (part) of route
 
+        if index is None: calculate distance through whole route
+
+        else if index is scalar: calculate distance from points before, at and immediately after indexed point and it's next adjacent point
+
+        else calculate distance between points specified in list
         """
         N = len(self.route)
         total = 0
         if index is None:
-            for n in range(N-1):
-                total += self.route[n].distance(self.route[n+1])
-            total = self.route[N-1].distance(self.route[0])
-        else:
-            index = [
+            rng = range(N)
+            total += self.route[N-1].distance(self.route[0])
+        elif not hasattr(index, "__len__"):
+            rng = [
                 ((index-1) % N),
                 (index % N),
                 ((index+1) % N),
                 ((index+2) % N)
                 ]
-            for n in index[:-1]:
-                total+=self.route[n].distance(self.route[n+1])
+            N = len(rng)
+        for n in range(N-1):
+            total += self.route[rng[n]].distance(self.route[rng[n+1]])
         return total
 
     def _calc_fitness(self) -> float:
         return 1/self.distance
 
-    def swap_direction(self, index: int) -> None:
+    def swap_direction(self, index: int) -> float:
         N = len(self.route)
         index1 = (index+1) % N
         old_dist = self._calc_distance(index)
         self.route[index], self.route[index1] = self.route[index1], self.route[index]  # Swap adjacent points on route
         new_dist = self._calc_distance(index)
-        self.distance += (new_dist - old_dist)
+        delta = new_dist - old_dist
+        self.distance += delta
         self.fitness = self._calc_fitness()
+        return delta
+
+    def plot(self) -> None:
+        x = []
+        y = []
+        for p in self.route:
+            x.append(p.x)
+            y.append(p.y)
+        x.append(self.route[0].x)
+        y.append(self.route[0].y)
+        return plt.plot(x, y, "o-")
 
 
 def generate_points(
@@ -83,7 +109,37 @@ def generate_points(
 
 
 def main(*args) -> None:
-    print(generate_points())
+    plt.ion()
+    N = 50
+    IterMax = 1000000000
+    route = Route(generate_points(N))
+    p = route.plot()
+    Str = str(route)
+    T = 1.
+    dT = 1e-3
+    dE = 0.
+    d = []
+    for n in range(IterMax):
+        indx = random.randint(0, N-1)
+        d.append(route.distance)
+        dE = N*route.swap_direction(indx)/route.distance
+        val = math.exp(-dE/T)
+        if val<=random.random():
+            route.swap_direction(indx)
+        if not (n%1000):
+            T = T*(1-dT)
+            Str = f"{n}: {route.distance:.3g}, T = {T:.3g}, dE = {dE:.3g}, {val:.3g}"
+            print(Str)
+            plt.gca().clear()
+            route.plot()
+            plt.title(Str)
+            plt.draw()
+            plt.pause(1e-5)
+
+    print(Str)
+    print(str(route))
+    route.plot()
+    plt.show()
 
 
 if __name__ == "__main__":
