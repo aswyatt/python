f64ac801e30b84c1367ce5aec59874a6942ccb16
Adam Wyatt <adam.wyatt@stfc.ac.uk>
2022-03-23 22:07:42 +0000

    Working - need to swap two "arbitrary" locations (non-adjacent)

--- TravellingSalesman/travelling_salesman.py
+++ TravellingSalesman/travelling_salesman.py
@@ -48,7 +48,7 @@ class Route:
 
         if index is None: calculate distance through whole route
 
-        else if index is scalar: calculate distance from points before, at and immediately after indexed point and it's next adjacent point
+        else if index is scalar: calculate distance from points immediately before and after current point to current point
 
         else calculate distance between points specified in list
         """
@@ -61,8 +61,7 @@ class Route:
             rng = [
                 ((index-1) % N),
                 (index % N),
-                ((index+1) % N),
-                ((index+2) % N)
+                ((index+1) % N)
                 ]
             N = len(rng)
         for n in range(N-1):
@@ -72,12 +71,13 @@ class Route:
     def _calc_fitness(self) -> float:
         return 1/self.distance
 
-    def swap_direction(self, index: int) -> float:
+    def swap_direction(self, index1: int, index2: int=None) -> float:
         N = len(self.route)
-        index1 = (index+1) % N
-        old_dist = self._calc_distance(index)
-        self.route[index], self.route[index1] = self.route[index1], self.route[index]  # Swap adjacent points on route
-        new_dist = self._calc_distance(index)
+        if index2 is None:
+            index2 = (index1+1) % N
+        old_dist = self._calc_distance(index1) + self._calc_distance(index2)
+        self.route[index1], self.route[index2] = self.route[index2], self.route[index1]  # Swap adjacent points on route
+        new_dist = self._calc_distance(index1) + self._calc_distance(index2)
         delta = new_dist - old_dist
         self.distance += delta
         self.fitness = self._calc_fitness()
@@ -115,26 +115,27 @@ def main(*args) -> None:
     route = Route(generate_points(N))
     p = route.plot()
     Str = str(route)
-    T = 1.
-    dT = 1e-3
+    T = .5
+    dT = .001
     dE = 0.
     d = []
     for n in range(IterMax):
-        indx = random.randint(0, N-1)
+        indx = random.sample(range(N), k=2)
         d.append(route.distance)
-        dE = N*route.swap_direction(indx)/route.distance
+        dE = N*route.swap_direction(indx[0], indx[1])/route.distance
         val = math.exp(-dE/T)
         if val<=random.random():
-            route.swap_direction(indx)
-        if not (n%1000):
+            route.swap_direction(indx[0], indx[1])
+        if not (n%100):
             T = T*(1-dT)
-            Str = f"{n}: {route.distance:.3g}, T = {T:.3g}, dE = {dE:.3g}, {val:.3g}"
+            Str = f"{n}: {route.distance:.2f}, T = {T:.4g}, dE = {dE:.4g}, {val:.3g}"
             print(Str)
             plt.gca().clear()
-            route.plot()
+            # route.plot()
+            plt.plot(d)
             plt.title(Str)
             plt.draw()
-            plt.pause(1e-5)
+            plt.pause(1e-3)
 
     print(Str)
     print(str(route))
