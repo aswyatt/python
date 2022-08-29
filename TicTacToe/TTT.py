b71e38a2f184c6d135eb7cf1843e82e643f2a8ac
Adam Wyatt <adam.wyatt@stfc.ac.uk>
2022-08-29 21:55:10 +0100

    Fixed some bugs

--- TicTacToe/TTT.py
+++ TicTacToe/TTT.py
@@ -18,6 +18,7 @@ class TTT:
             raise ValueError(f"Invalid move: [{row}, {col}] is not empty")
         self.Grid[row, col] = self.CurrentPlayer
         self.CurrentPlayer = -self.CurrentPlayer
+        self.Turn += 1
 
     def CheckForWinner(self) -> int:
         G = self.Grid
@@ -25,12 +26,12 @@ class TTT:
         AD = np.fliplr(G).diagonal().reshape((-1,1))
         S = np.hstack((G, G.T, D, AD)).sum(0)
         S = G.sum(0)
-        if np.any(S==3):
+        if self.Turn>9:
+            return 0
+        elif np.any(S==3):
             return 1
         elif np.any(S==-3):
             return -1
-        elif np.Turn>9:
-            return 0
         else:
             return None
 
@@ -43,12 +44,18 @@ def main():
     T = TTT()
     r = 0
     c = 1
-    for n in range(1):
+    W = None
+    while W is None:
         while T.Grid[r, c]:
             r = random.randrange(3)
             c = random.randrange(3)
         T.Play(r, c)
         T.Display()
+        W = T.CheckForWinner()
+    if W==0:
+        print("Draw")
+    else:
+        print("Winner is player", W)
 
 if __name__=="__main__":
     main()
